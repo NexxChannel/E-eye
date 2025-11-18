8786a4ff778874625a918253b81a4a1b95743e39
Mark20110927 <13261317705@sina.cn>
2025-11-18 19:07:10 +0800

    封装数据库操作

---
+++ backend/app/crud.py
@@ -0,0 +1,22 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+from argon2 import PasswordHasher
+
+Hasher = PasswordHasher()
+
+def hashPassword(rawPassword: str) -> str:
+    return Hasher.hash(rawPassword)
+
+def gerUserByEmail(db: Session, email: str) -> models.User | None:
+    return db.query(models.User).filter(models.User.email == email).first()
+
+def createUser(db: Session, userIn: schemas.UserCreate) -> models.User:
+    hashed = hashPassword(userIn.password)
+    dbUser = models.User(
+        email=userIn.email,
+        hashedPassword=hashed
+    )
+    db.add(dbUser)
+    db.commit()
+    db.refresh(dbUser)
+    return dbUser
