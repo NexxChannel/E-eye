94462d7699f0cda1484036b38adae3ad2f40a465
Mark20110927 <13261317705@sina.cn>
2025-11-18 19:06:27 +0800

    数据库连接&基类

---
+++ backend/app/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./e_eye.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(
+    autoflush=False,
+    autocommit=False,
+    bind=engine,
+)
