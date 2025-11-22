01d03a27527011ca8a0d538c4c2129076e8e08ef
Mark20110927 <13261317705@sina.cn>
2025-11-22 16:12:26 +0800

    添加前端配置文件

---
+++ frontend/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '/users': 'http://127.0.0.1:8000',
+      '/auth': 'http://127.0.0.1:8000',
+      '/me': 'http://127.0.0.1:8000',
+      '/health': 'http://127.0.0.1:8000',
+    }
+  }
+})
