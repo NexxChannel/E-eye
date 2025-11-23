6d207885797168d0e9ff126bf7aa52cff0c15951
Mark20110927 <13261317705@sina.cn>
2025-11-23 16:48:00 +0800

    添加Project模型

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,5 +1,8 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+
 from .database import Base
+from datetime import datetime, UTC
 
 
 class User(Base):
@@ -11,3 +14,13 @@ class User(Base):
     isActive = Column(Boolean, default=True)
     subscriptionLevel = Column(String, default="free")
     role = Column(String, default="user")
+    projects = relationship("Project", back_populates="owner")
+
+class Project(Base):
+    __tablename__ = "projects"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    createdAt = Column(DateTime, default=datetime.now(UTC))
+    ownerId = Column(Integer, ForeignKey("users.id"), nullable=False)
+    owner = relationship("User", back_populates="projects")
